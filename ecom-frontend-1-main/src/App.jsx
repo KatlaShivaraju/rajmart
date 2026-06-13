2fa0a5b0cc310184e7974e2a90fe93f17bc0cab1
Shivaraju Katla <shivaraj83200@gmail.com>
2026-06-13 14:51:36 +0530

    Add files via upload
    Completed ecommerce auth cart flow"

--- ecom-frontend-1-main/src/App.jsx
+++ ecom-frontend-1-main/src/App.jsx
@@ -1,41 +1,18 @@
-import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProductDetails from "./components/ProductDetails";
-
-import Navbar from "./components/Navbar";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 import Home from "./components/Home";
-import AddProduct from "./components/AddProduct";
+import Cart from "./Pages/Cart";
 
 function App() {
-  // 🛒 Global Cart State (shared with Navbar + Home)
-  const [cart, setCart] = useState([]);
-
   return (
     <BrowserRouter>
-      
-      {/* ✅ Navbar gets cart count */}
-      <Navbar cartCount={cart.length} />
-
-      {/* ✅ Routes */}
       <Routes>
-        
-        {/* 🏠 Home Page */}
-        <Route
-          path="/"
-          element={
-            <Home cart={cart} setCart={setCart} />
-          }
-        />
-
-        {/* ➕ Add Product Page */}
-        <Route path="/add_product" element={<AddProduct />} />
-        <Route
-  path="/product/:id"
-  element={<ProductDetails cart={cart} setCart={setCart} />}
-/>
-
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
