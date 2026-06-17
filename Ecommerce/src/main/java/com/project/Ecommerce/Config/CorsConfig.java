dede2bf3d542d33422fc3d3c9164127d74955515
KatlaShivaraju <shivaraj83200@gmail.com>
2026-06-18 01:16:11 +0530

    Add CORS config

--- Ecommerce/src/main/java/com/project/Ecommerce/Config/CorsConfig.java
+++ Ecommerce/src/main/java/com/project/Ecommerce/Config/CorsConfig.java
@@ -2,38 +2,26 @@ package com.project.Ecommerce.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer
-    corsConfigurer() {
-
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
-            public void addCorsMappings(
-                    CorsRegistry registry
-            ) {
+            public void addCorsMappings(CorsRegistry registry) {
 
-                registry
-                        .addMapping("/**")
-
+                registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "http://localhost:5174",
-                                "http://localhost:5175"
+                                "https://rajmartt.netlify.app"
                         )
-
-                        .allowedMethods(
-                                "*"
-                        )
-
-                        .allowedHeaders(
-                                "*"
-                        );
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
